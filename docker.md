b83792ff252c5dc680a21faf88d7125086347b1c
jim <jbwhite2@ncsu.edu>
2022-02-17 12:04:25 -0500

    docker md

---
+++ docker.md
@@ -0,0 +1,2 @@
+* 2022-01-19
+Merge version 2.3 into docker.
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     markdown = require('gulp-markdown');
 
-gulp.task('markdown_docker', function() {
+gulp.task('markdown_tbas', function() {
     return gulp.src(["versions.md", "history.md"])
         .pipe(rename({
             extname: ".html",
@@ -14,13 +14,25 @@ gulp.task('markdown_docker', function() {
         .pipe(gulp.dest('/var/www/html/tbas2_3/pages'))
 });
 
+gulp.task('markdown_docker', function() {
+    return gulp.src(["docker.md"])
+        .pipe(rename({
+            extname: ".html",
+            suffix: "-history"
+
+        }))
+        .pipe(markdown())
+
+        .pipe(gulp.dest('/var/www/docker/tbas2/pages'))
+});
+
 gulp.task('move_images', function() {
     return gulp.src("images/tbas-history/*.*")
         .pipe(gulp.dest('/var/www/html/tbas2_3/pages/images/tbas-history'));
 });
 
-// gulp.task('default', ['markdown_docker', 'move_images']);
+// gulp.task('default', ['markdown_tbas', 'move_images']);
 
 exports.default = gulp.series(
-    'markdown_docker', 'move_images'
+    'markdown_tbas', 'move_images', 'markdown_docker'
 );
